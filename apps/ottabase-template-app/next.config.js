edbb94d09bac5f5f220a7c80ddd1b8fcd9b5a0ab
Deepak Thomas <think.dj@gmail.com>
2025-09-20 11:34:31 +0530

    update ottabase-template-app: next.config

--- apps/ottabase-template-app/next.config.js
+++ apps/ottabase-template-app/next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
         '@ottabase/ui-code-highlight',
         '@ottabase/ui-tailwind',
         '@ottabase/ui-components',
+        '@ottabase/utils',
+        '@ottabase/core-prisma',
+        '@ottabase/core-auth',
     ],
 
     // Enable React strict mode
@@ -22,7 +25,41 @@ const nextConfig = {
     // Experimental features for Next.js 15
     experimental: {
         // Add any Next.js 15 experimental features here
+        cssChunking: true, // default true
+        optimizeCss: true, // default true
+        optimizeFonts: true, // default true
+        optimizePackageImports: ['@mantine/core', '@mantine/hooks', '@mantine/carousel'],
     },
+    compiler: {
+        // Enables the styled-components SWC transform
+        // Warning: Prop `className` did not match. Server: "mantine-xxx mantine-Switch-track" Client: "mantine-yyy mantine-Switch-track"
+        styledComponents: true,
+    },
+    devIndicators: {
+        position: 'bottom-right',
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+    // Next.js 15
+    // swcMinify: true,
+    sassOptions: {
+        //silenceDeprecations: ["legacy-js-api"], // Migrated SaaS
+    },
+    // Allowed development origins for API routes
+    // e.g.: '*.local-origin.dev'
+    // If using tunnels (e.g., ngrok.io) or other custom dev TLDs, you can add them via NEXT_ALLOWED_DEV_ORIGINS (comma separated)
+    allowedDevOrigins: [
+        ...(process.env.NEXT_ALLOWED_DEV_ORIGINS?.split(',') ?? []),
+    ],
+    serverExternalPackages: [
+        'fs', // For Email Templates compiler
+    ],
 }
 
 module.exports = nextConfig
